495c22724e5cf576d6b2d34cbafc69e08fe070f6
Ninja214 <krishna.p.gundam@gmail.com>
2015-08-04 14:28:40 +0200

    FirstTime
    Added new files

---
+++ src/java/org/mapmybus/track/entity/Bus.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mapmybus.track.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author z6kgn
+ */
+@Entity
+@Table(name = "BUS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Bus.findAll", query = "SELECT b FROM Bus b"),
+    @NamedQuery(name = "Bus.findById", query = "SELECT b FROM Bus b WHERE b.id = :id"),
+    @NamedQuery(name = "Bus.findByLongitude", query = "SELECT b FROM Bus b WHERE b.longitude = :longitude"),
+    @NamedQuery(name = "Bus.findByLatitude", query = "SELECT b FROM Bus b WHERE b.latitude = :latitude")})
+public class Bus implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "LONGITUDE")
+    private Float longitude;
+    @Column(name = "LATITUDE")
+    private Float latitude;
+
+    public Bus() {
+    }
+
+    public Bus(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Bus)) {
+            return false;
+        }
+        Bus other = (Bus) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.mapmybus.track.entity.Bus[ id=" + id + " ]";
+    }
+    
+}
